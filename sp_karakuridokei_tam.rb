af16ce253263ef15299f21b6013ccbe0dae6ebff
xxhaaaaaxx <158167225+xxhaaaaaxx@users.noreply.github.com>
2024-02-07 01:12:16 +0900

    Add files via upload

---
+++ sp_karakuridokei_tam.rb
@@ -0,0 +1,320 @@
+# 採譜元クレジット
+# TAM Music Factory
+# https://www.tam-music.com/
+# より
+# オルゴール『カラクリ時計』
+# https://www.tam-music.com/m000_category/musicbox
+
+# 楽曲素材利用規定(https://www.tam-music.com/interface)を遵守し作成しております
+
+use_bpm 100
+
+2.times do
+  in_thread do
+    use_synth :pretty_bell
+    play 79
+    sleep 0.25
+    play 80
+    sleep 0.25
+    play 82
+    sleep 0.25
+    play 84
+    sleep 0.25
+    play 82
+    sleep 0.50
+    play 75
+    sleep 0.50
+    play 77
+    sleep 0.50
+    play 72
+    sleep 0.50
+    play 77
+    sleep 1
+    #
+    play 77
+    sleep 0.25
+    play 79
+    sleep 0.25
+    play 80
+    sleep 0.25
+    play 82
+    sleep 0.25
+    play 80
+    sleep 0.50
+    play 77
+    sleep 0.50
+    play 79
+    sleep 1
+    play 79
+    sleep 1
+    #
+    play 79
+    sleep 0.25
+    play 80
+    sleep 0.25
+    play 82
+    sleep 0.25
+    play 84
+    sleep 0.25
+    play 82
+    sleep 0.50
+    play 75
+    sleep 0.50
+    play 77
+    sleep 0.50
+    play 72
+    sleep 0.50
+    play 77
+    sleep 1
+    #
+    play 77
+    sleep 0.25
+    play 79
+    sleep 0.25
+    play 80
+    sleep 0.25
+    play 82
+    sleep 0.25
+    play 80
+    sleep 0.50
+    play 77
+    sleep 0.50
+    play 75
+    sleep 1
+    play 87
+    sleep 0.50
+    play 86
+    sleep 0.50
+    #後半
+    play 86
+    sleep 0.50
+    play 84
+    sleep 0.25
+    play 86
+    sleep 0.25
+    play 87
+    sleep 0.50
+    play 89
+    sleep 0.50
+    play 87
+    sleep 0.50
+    play 82
+    sleep 1
+    play 79
+    sleep 0.5
+    #
+    play 80
+    sleep 0.25
+    play 79
+    sleep 0.25
+    play 80
+    sleep 0.25
+    play 82
+    sleep 0.25
+    play 80
+    sleep 0.50
+    play 77
+    sleep 0.50
+    play 79
+    sleep 0.50
+    play 82
+    sleep 0.50
+    play 75
+    sleep 0.50
+    play 87
+    sleep 0.50
+    #
+    play 86
+    sleep 0.50
+    play 84
+    sleep 0.25
+    play 86
+    sleep 0.25
+    play 87
+    sleep 0.50
+    play 89
+    sleep 0.50
+    play 87
+    sleep 0.50
+    play 82
+    sleep 1
+    play 79
+    sleep 0.5
+    #
+    play 80
+    sleep 0.25
+    play 79
+    sleep 0.25
+    play 80
+    sleep 0.25
+    play 82
+    sleep 0.25
+    play 80
+    sleep 0.50
+    play 77
+    sleep 0.50
+    play 75
+    sleep 0.50
+    play 82
+    sleep 0.50
+    play 75
+    sleep 1
+  end
+  
+  # 伴奏
+  use_synth :pretty_bell
+  play 63
+  sleep 0.50
+  play 70
+  sleep 0.50
+  play 67
+  sleep 0.50
+  play 70
+  sleep 0.50
+  
+  play 63
+  sleep 0.50
+  play 72
+  sleep 0.50
+  play 68
+  sleep 0.50
+  play 72
+  sleep 0.50
+  
+  play 62
+  sleep 0.50
+  play 65
+  sleep 0.50
+  play 65
+  sleep 0.50
+  play 70
+  sleep 0.50
+  
+  play 63
+  sleep 0.50
+  play 70
+  sleep 0.50
+  play 67
+  sleep 0.50
+  play 65
+  sleep 0.50
+  
+  
+  play 63
+  sleep 0.50
+  play 70
+  sleep 0.50
+  play 67
+  sleep 0.50
+  play 70
+  sleep 0.50
+  
+  play 63
+  sleep 0.50
+  play 72
+  sleep 0.50
+  play 68
+  sleep 0.50
+  play 72
+  sleep 0.50
+  
+  play 62
+  sleep 0.50
+  play 65
+  sleep 0.50
+  play 65
+  sleep 0.50
+  play 70
+  sleep 0.50
+  
+  play 63
+  sleep 0.50
+  play 70
+  sleep 0.50
+  play 67
+  sleep 1
+  
+  play 63
+  sleep 0.50
+  play 72
+  sleep 0.50
+  play 68
+  sleep 0.25
+  play 70
+  sleep 0.25
+  play 72
+  sleep 0.5
+  
+  play 63
+  sleep 0.50
+  play 70
+  sleep 0.50
+  play 67
+  sleep 0.50
+  play 70
+  sleep 0.50
+  
+  play 62
+  sleep 0.50
+  play 70
+  sleep 0.50
+  play 65
+  sleep 0.25
+  play 67
+  sleep 0.25
+  play 68
+  sleep 0.25
+  play 65
+  sleep 0.25
+  
+  play 63
+  sleep 0.50
+  play 63
+  sleep 0.50
+  play 67
+  sleep 0.50
+  play 70
+  sleep 0.50
+  
+  
+  
+  play 63
+  sleep 0.50
+  play 72
+  sleep 0.50
+  play 68
+  sleep 0.50
+  play 72
+  sleep 0.50
+  
+  play 63
+  sleep 0.50
+  play 70
+  sleep 0.50
+  play 67
+  sleep 0.50
+  play 70
+  sleep 0.50
+  
+  play 62
+  sleep 0.50
+  play 70
+  sleep 0.50
+  play 65
+  sleep 0.25
+  play 67
+  sleep 0.25
+  play 68
+  sleep 0.25
+  play 65
+  sleep 0.25
+  
+  play 63
+  sleep 0.50
+  play 70
+  sleep 0.50
+  play 67
+  sleep 0.50
+  play 70
+  sleep 0.50
+end
